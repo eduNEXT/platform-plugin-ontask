66ba0b18e89db15396d728d8f7dd2af070eb30e4
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-05-17 13:40:38 -0400

    fix: remove import that breaks production environment

--- platform_plugin_ontask/__init__.py
+++ platform_plugin_ontask/__init__.py
@@ -4,8 +4,6 @@ Open edX Django plugin for OnTask Learning.
 import os
 from pathlib import Path
 
-from .extensions.filters import AddInstructorOnTaskTab
-
 __version__ = "0.2.0"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
