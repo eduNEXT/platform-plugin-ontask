3cb82cd179a3f38cc18d7fa223582ce4aefc9425
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-07-11 08:44:03 -0500

    chore: add update_item backend to update draft and published branches

--- platform_plugin_ontask/edxapp_wrapper/modulestore.py
+++ platform_plugin_ontask/edxapp_wrapper/modulestore.py
@@ -15,3 +15,13 @@ def modulestore(*args, **kwargs):
     backend = import_module(backend_function)
 
     return backend.modulestore(*args, **kwargs)
+
+
+def update_item(*args, **kwargs):
+    """
+    Wrapper for `xmodule.modulestore.django.modulestore.update_item`
+    """
+    backend_function = settings.PLATFORM_PLUGIN_ONTASK_MODULESTORE_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.update_item(*args, **kwargs)
