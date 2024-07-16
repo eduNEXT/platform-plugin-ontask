341c363106b31a9d27c642e72fce30857675a451
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-07-16 16:11:18 -0500

    test: update unit tests according to latest changes

--- platform_plugin_ontask/tests/test_utils.py
+++ platform_plugin_ontask/tests/test_utils.py
@@ -2,18 +2,15 @@
 
 from unittest import TestCase
 
-from ddt import data, ddt, unpack
 from django.test import override_settings
-from rest_framework import status
 
-from platform_plugin_ontask.api.utils import api_error, get_data_summary_class
+from platform_plugin_ontask.api.utils import get_data_summary_class
 from platform_plugin_ontask.datasummary.backends.completion import CompletionDataSummary
 from platform_plugin_ontask.datasummary.backends.tests.dummy import DummyDataSummary
 
 DUMMY_DATA_SUMMARY_CLASS = "platform_plugin_ontask.datasummary.backends.tests.dummy.DummyDataSummary"
 
 
-@ddt
 class TestUtils(TestCase):
     """Tests for the utility functions in the OnTask plugin."""
 
@@ -26,16 +23,3 @@ class TestUtils(TestCase):
     def test_custom_class_return(self):
         result = get_data_summary_class()
         self.assertIs(result, DummyDataSummary)
-
-    @data(
-        ("Custom field error message", status.HTTP_400_BAD_REQUEST),
-        ("Another custom field error message", status.HTTP_404_NOT_FOUND),
-        ("Yet another custom field error message", status.HTTP_500_INTERNAL_SERVER_ERROR),
-        ({"error": "Custom field error message"}, status.HTTP_400_BAD_REQUEST),
-    )
-    @unpack
-    def test_api_error(self, error: str, status_code: int):
-        response = api_error(error, status_code)
-
-        self.assertEqual(response.status_code, status_code)
-        self.assertDictEqual(response.data, {"error": error})
--- platform_plugin_ontask/tests/test_views.py
+++ platform_plugin_ontask/tests/test_views.py
@@ -100,7 +100,7 @@ class OnTaskWorkflowAPIViewTest(APITestCase):
 
         response = self.post_request()
 
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data["error"],
             "An error occurred while creating the workflow. Ensure the "
