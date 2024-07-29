970491d0848c1ff49863f827bfa84a4146465778
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-07-29 16:53:02 -0500

    test: update unit tests according to latest changes

--- platform_plugin_ontask/edxapp_wrapper/backends/tests/enrollments_r_v1_test.py
+++ platform_plugin_ontask/edxapp_wrapper/backends/tests/enrollments_r_v1_test.py
@@ -2,4 +2,15 @@
 Enrollments test definitions for Open edX Redwood release.
 """
 
-get_user_enrollments = object
+from unittest.mock import Mock
+
+
+def get_user_enrollments(*args, **kwargs):
+    """
+    update_item test backend.
+    """
+    return [
+        Mock(user=Mock(id=1)),
+        Mock(user=Mock(id=2)),
+        Mock(user=Mock(id=3)),
+    ]
--- platform_plugin_ontask/settings/test.py
+++ platform_plugin_ontask/settings/test.py
@@ -76,3 +76,5 @@ PLATFORM_PLUGIN_ONTASK_ENROLLMENTS_BACKEND = (
 )
 PLATFORM_PLUGIN_ONTASK_COMPLETION_BACKEND = "platform_plugin_ontask.edxapp_wrapper.backends.tests.completion_r_v1_test"
 ONTASK_DATA_SUMMARY_CLASSES = ["platform_plugin_ontask.datasummary.backends.tests.dummy.DummyDataSummary"]
+ONTASK_API_AUTH_TOKEN = "ontask-api-auth-token"
+ONTASK_INTERNAL_API="http://ontask:8080"
