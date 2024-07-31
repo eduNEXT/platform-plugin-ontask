dda12b9e0f5383125ca1083a3b7cc5c78ed8ddc8
Bryann Valderrama <bryann.valderrama@edunext.co>
2024-07-31 12:51:37 -0500

    test: add new tasks unit tests

--- platform_plugin_ontask/tests/test_tasks.py
+++ platform_plugin_ontask/tests/test_tasks.py
@@ -3,6 +3,7 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+from django.test.utils import override_settings
 from rest_framework import status
 
 from platform_plugin_ontask.api.utils import ontask_log_from_response
@@ -34,3 +35,21 @@ class TestUploadDataframeOnTaskTask(TestCase):
 
         mock_merge_table.assert_called_once_with(self.workflow_id, DummyDataSummary(self.course_id).get_data_summary())
         mock_log.info.assert_called_with(ontask_log_from_response(mock_merge_table.return_value))
+
+    @override_settings(ONTASK_DATA_SUMMARY_CLASSES=[])
+    @patch(f"{TASKS_MODULE_PATH}.log")
+    def test_upload_dataframe_to_ontask_data_summary_classes_not_set(self, mock_log: Mock):
+        """Test uploading a dataframe to OnTask when ONTASK_DATA_SUMMARY_CLASSES is not set."""
+        upload_dataframe_to_ontask_task(self.course_id, self.workflow_id, self.api_auth_token)
+
+        mock_log.info.assert_called_with("ONTASK_DATA_SUMMARY_CLASSES is not set.")
+
+    @override_settings(ONTASK_DATA_SUMMARY_CLASSES=["non.existent.path"])
+    @patch(f"{TASKS_MODULE_PATH}.OnTaskClient.merge_table")
+    @patch(f"{TASKS_MODULE_PATH}.log")
+    def test_upload_dataframe_to_ontask_data_summary_class_not_found(self, mock_log: Mock, mock_merge_table: Mock):
+        """Test uploading a dataframe to OnTask when the data summary class is not found."""
+        upload_dataframe_to_ontask_task(self.course_id, self.workflow_id, self.api_auth_token)
+
+        mock_log.error.assert_called_with("Data summary class non.existent.path not found.")
+        mock_merge_table.assert_not_called()
